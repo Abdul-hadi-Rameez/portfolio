ffbab335c7be9a2aa59074e03b8f76c0979f1b4b
Abdul-hadi-rameez <abdulhadirameez@gmail.com>
2024-11-26 13:27:38 +0530

    portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { EducationComponent } from './education/education.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    EducationComponent,
+    PersonalInformationComponent,
+    SkillsComponent,
+    ContactComponent,
+    ProjectsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/personal-information/personal-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-information',
+  standalone: false,
+  
+  templateUrl: './personal-information.component.html',
+  styleUrl: './personal-information.component.css'
+})
+export class PersonalInformationComponent {
+
+}
